c42c35d3f782500aa9ac05e9f9a18fb9f791a608
Alex <rollinglex@gmail.com>
2017-11-01 18:02:14 +0900

    header responsive

--- grocery_list/src/App.js
+++ grocery_list/src/App.js
@@ -3,15 +3,14 @@ import React, { Component } from "react";
 import Filter from "./TopLevel/Filter";
 import List from "./TopLevel/List";
 import AddItemContainer from "./containers/AddItemContainer";
+import Header from "./components/Header";
 import logo from "./logo.svg";
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<div className="jumbotron">
-					<h1 className="display-3 text-center">Shopping list</h1>
-				</div>
+				<Header />
 				<AddItemContainer />
 				<Filter />
 				<List />
